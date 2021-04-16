52f62119d4b37740bd5b77d5e73d12d3fc0669fc
Daniel Guarchaj <gtdanniel@gmail.com>
2021-04-15 23:50:52 -0600

    Create models

--- crm_client/admin.py
+++ crm_client/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Departamento,
+    Cliente,
+    Producto,
+    Venta,
+    DetalleVenta,
+    Proveedor,
+    Compra,
+    DetalleCompra,
+)
+
+admin.site.register(Departamento)
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Venta)
+admin.site.register(DetalleVenta)
+admin.site.register(Proveedor)
+admin.site.register(Compra)
+admin.site.register(DetalleCompra)
--- crm_client/models.py
+++ crm_client/models.py
@@ -1,3 +1,99 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+ESTADOS = (
+    (0, 'Inactivo'),
+    (1, 'Activo'),
+)
+
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=150)
+
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+
+class Cliente(models.Model):
+    nombres = models.CharField(max_length=150)
+    apellidos = models.CharField(max_length=150)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    direccion = models.CharField(max_length=150, blank=True, null=True)
+    telefono = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    codigo = models.CharField(max_length=15, blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+
+
+class Producto(models.Model):
+    codigo = models.CharField(max_length=150)
+    nombre = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=150, blank=True, null=True)
+    precio_venta = models.DecimalField(max_digits=8, decimal_places=2)
+    stock = models.IntegerField()
+    fecha_vencimiento = models.DateField(blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+
+class Venta(models.Model):
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    numero_factura = models.CharField(max_length=150)
+    comentarios = models.TextField(blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    vendedor = models.ForeignKey(User, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+
+class DetalleVenta(models.Model):
+    cantidad = models.IntegerField()
+    precio_venta = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+
+
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=150)
+    direccion = models.CharField(max_length=150, blank=True, null=True)
+    telefono = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    codigo = models.CharField(max_length=15, blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+
+class Compra(models.Model):
+    total = models.DecimalField(max_digits=8, decimal_places=2)
+    numero_factura = models.CharField(max_length=150)
+    comentarios = models.TextField(blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+
+
+class DetalleCompra(models.Model):
+    cantidad = models.IntegerField()
+    costo_unitario = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
+    estado = models.SmallIntegerField(choices=ESTADOS, default=1)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+
+    compra = models.ForeignKey(Compra, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
