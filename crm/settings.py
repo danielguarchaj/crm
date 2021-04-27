cfd0a8acc2922cd3ed5f6a39acfacde86265f690
Daniel Guarchaj <gtdanniel@gmail.com>
2021-04-26 23:03:19 -0600

    Add database creds

--- crm/settings.py
+++ crm/settings.py
@@ -76,11 +76,31 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd3inedsjcu67nt',
+
+        'USER': 'usfjbkgnegjmpk',
+
+        'PASSWORD': 'e50ec45b8e1b73f198054fe6417274763912e9bb96a79937c9fdbc27de433487',
+
+        'HOST': 'ec2-107-20-153-39.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
